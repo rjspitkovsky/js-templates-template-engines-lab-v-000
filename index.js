f3f5ae6fd63b6f5887b68be3a4d47c7a1aa6f9bd
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-10 19:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,21 +12,29 @@ function createPost(){
   // Create page area for posts
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate()
 
-  // Create
+  // Create post and comments based on template
   let postSection = postTemplate({ 'title': postTitle, 'body': postBody, 'poster': postAuthor });
   let commentsSection = commentsTemplate();
+
+  // create post area for post
   let postElement = document.getElementById("post");
 
+  // insert created post in post area and create comments in comment area
   postElement.innerHTML = postSection;
   postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
 function postComment() {
+  // Establish values for individual comment
   let commenter = document.getElementById("commenter-name").value
   let comment = document.getElementById("comment").value
 
+  // Create template for comment
   let commentTemplate = _.template(document.getElementById("comment-template").innerHTML)
 
+  // Create comment area for comments
   let commentDiv = document.getElementById("comments")
+
+  // insert comment in comment area 
   commentDiv.innerHTML += commentTemplate({"commenter": commenter, "comment": comment})
 }
