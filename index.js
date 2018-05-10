8a882dea6a0a739eb508268408edfb7dccb1a05f
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-10 19:01:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function createPost() {
+  const title = document.getElementById("post-title").innerHTML
+  const author = document.getElementById("post-author").innerHTML 
+  const body = document.getElementById("post-body")
+}
