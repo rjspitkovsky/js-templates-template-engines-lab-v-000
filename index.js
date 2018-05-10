e4d68125c9231bd6fb5fb43f3b7d6726c512816c
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-10 19:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// function createPost() {
-//   let postTemplateFn = _.template(document.getElementById("post-template").innerHTML)
-//   let pageTemplateFn = _.template(document.getElementById("page-template").innerHTML)
-//   let commentsTemplateFn = _.template(document.getElementById("comments-template").innerHTML)
-//
-//
-//   let title = document.getElementById("post-title").innerHTML
-//   let author = document.getElementById("post-author").innerHTML
-//   let body = document.getElementById("post-body").innerHTML
-//
-//   document.getElementsByTagName("main")[0].innerHTML += pageTemplateFn()
-//
-//   let postAttributes = postTemplateFn({post-title: title, post-author: author, post-body: body})
-//   let comments = commentsTemplateFn()
-//
-//   let postArea = document.getElementById("post")
-//   postArea.innerHTML = postAttributes
-//   postArea.getElementsByTagName("footer")[1].innerHTML = comments
-// }
-
-
 function createPost(){
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
   var postTemplate = _.template(document.getElementById("post-template").innerHTML)
