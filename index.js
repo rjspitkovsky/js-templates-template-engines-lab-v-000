d1941bb10b095494f2f316ba960835bf4a13598e
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-10 19:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function createPost() {
   const title = document.getElementById("post-title").innerHTML
   const author = document.getElementById("post-author").innerHTML
   const body = document.getElementById("post-body").innerHTML
+
+  const postTemplate = _.template(document.getElementById("post-template").innerHTML)
 }
